7bee07def42da014b47f66cd99ccd5cccf9e3d84
h8art <h8art@mail.ru>
2020-03-25 12:45:03 +0300

    ffixx

--- src/store/actions/calendar.js
+++ src/store/actions/calendar.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 export default {
   getCalendar: ({ commit }, payload) => {
     commit('cleanCalendar');
-    axios.get(`/tasks/calendar?year=${payload.year}&month=${payload.month}`).then((res) => {
+    let month = 0;
+    if (payload.month.length < 2) month = `0${payload.month}`;
+    else month = payload.month;
+    axios.get(`/calendar?year=${payload.year}&month=${month}`).then((res) => {
       commit('updateCalendar', res.data.calendar);
     });
   },
